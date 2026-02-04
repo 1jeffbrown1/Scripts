81193a1c91a46b61cbb90c26c2ea524ad054126f
1jeffbrown1 <xecros@gmail.com>
2026-02-04 14:04:55 -0800

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,5 @@
 alias ls="ls -Cahgl --group-directories-first --color=auto"
 alias cls="clear"
 alias grep='grep --color=auto'
+alias kgap='k get pods -A -o='custom-columns=Name:{.metadata.name},Namespace:{.metadata.namespace},Node:{.spec.nodeName},Status:{.status.phase}' --sort-by=.metadata.namespace'
+
